daefa1e3aa2ef0079fb5dcb091cfbfd3b3bca290
barrie9 <one.action.613@gmail.com>
2020-08-02 09:17:44 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,17 +46,12 @@ class TasksController < ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
 
-private
-
-def set_task
-  @task = Task.find(params[:id])
-end
-
-def message_params
-  params.require(:task).permit(:content)
-end
